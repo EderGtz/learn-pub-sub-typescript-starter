689d80ca04fa1fdf585344a042880f4f276c64ca
Eder Gutierrez <gutierrezeder64@gmail.com>
2026-05-10 10:51:16 -0600

    feat: update RabbitMQ connection handling and dependencies

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,24 @@
+import amqp from "amqplib";
+import { env, exit } from 'node:process';
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = env.RABBITMQ_CONNECTION;
+  const conn = await amqp.connect(rabbitConnString!);
+  console.log("Connection with RabbitMQ successful!");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
